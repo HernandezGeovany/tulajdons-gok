fc15ed73b59a6395479d51f8c2e803448296f050
HernandezGeovany <94844065+HernandezGeovany@users.noreply.github.com>
2023-12-07 08:33:37 +0100

    Update geci.py

--- geci.py
+++ geci.py
@@ -7,17 +7,8 @@ title = st.text_input('email', '')
 title = st.text_input('name', '')
 
 
-today = datetime.datetime.now()
-next_year = today.year + 1
-jan_1 = datetime.date(next_year, 1, 1)
-dec_31 = datetime.date(next_year, 12, 31)
-
-d = st.date_input(
-    "Select your vacation for next year",
-    (jan_1, datetime.date(next_year, 1, 7)),
-    jan_1,
-    dec_31,
-    format="MM.DD.YYYY",
+d = st.date_input("When's your birthday", value=None)
+st.write('Your birthday is:', d
 )
 
 genre = st.radio(
