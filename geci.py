ab90c63cc4d97544de2bac9d0df6bb438b98dcdc
HernandezGeovany <94844065+HernandezGeovany@users.noreply.github.com>
2023-12-07 08:28:22 +0100

    geci.py

--- geci.py
+++ geci.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
 title = st.text_input('email', '')
+
+title = st.text_input('name', '')
+
+genre = st.radio(
+    "What's your gender",
+    ["mail", "Female"],
+    index=None,
+)
+
+
+
