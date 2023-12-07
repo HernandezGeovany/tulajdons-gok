79ac8be04b9267c62e9799832bda8c1d2f0da6d5
HernandezGeovany <94844065+HernandezGeovany@users.noreply.github.com>
2023-12-07 08:32:59 +0100

    Update geci.py

--- geci.py
+++ geci.py
@@ -1,10 +1,24 @@
+
+import datetime
 import streamlit as st
 
 title = st.text_input('email', '')
 
 title = st.text_input('name', '')
 
-title = st.text_input('how old are you', '')
+
+today = datetime.datetime.now()
+next_year = today.year + 1
+jan_1 = datetime.date(next_year, 1, 1)
+dec_31 = datetime.date(next_year, 12, 31)
+
+d = st.date_input(
+    "Select your vacation for next year",
+    (jan_1, datetime.date(next_year, 1, 7)),
+    jan_1,
+    dec_31,
+    format="MM.DD.YYYY",
+)
 
 genre = st.radio(
     "What's your gender",
